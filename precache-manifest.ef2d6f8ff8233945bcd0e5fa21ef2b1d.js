c474fb75c5960f6a25a783e14b37f1a3e452e37b
Anton <zalaldinov@icloud.com>
2019-11-20 17:24:51 +0300

    Updates

---
+++ precache-manifest.ef2d6f8ff8233945bcd0e5fa21ef2b1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23727973ced16cf0b3e735eed708bdb",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "01dc8c73a291d0930407",
+    "url": "/monster-rolodex/static/css/main.ca9d154b.chunk.css"
+  },
+  {
+    "revision": "54287a5e279147dbf894",
+    "url": "/monster-rolodex/static/js/2.d52c949a.chunk.js"
+  },
+  {
+    "revision": "01dc8c73a291d0930407",
+    "url": "/monster-rolodex/static/js/main.acd359a4.chunk.js"
+  },
+  {
+    "revision": "1afc0d6c9e1d3ea58cfb",
+    "url": "/monster-rolodex/static/js/runtime-main.d6746b7e.js"
+  }
+]);
